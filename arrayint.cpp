09e2160e9c0a8838b4182100e05ddfc58a74a854
135777Derick <117374653+135777Derick@users.noreply.github.com>
2022-12-01 20:03:45 +0300

    Update arrayint.cpp

--- arrayint.cpp
+++ arrayint.cpp
@@ -38,3 +38,28 @@ int main() {
     }
     return 0;
 }
+
+
+// summation & product & mean of arrays
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int arraySize = 5;
+    int nums [arraySize];
+    cout<<"please enter 5 interger values "<<endl;
+    for(int i=0; i<5 ; i++){
+        cin>>nums[i];
+    }
+    int sum = 0, product =1,mean;
+    for(int i=0; i<5 ; i++){
+        sum = sum + nums[i];
+        product =product * nums[i];
+    }
+    cout<<"the sum of the array values you entered is ; "<<sum<<endl;
+    cout<<"the product of the array values you entered is ; "<<product<<endl;
+    mean = sum/ arraySize;
+    cout<<"the mean of the array values you entered is ; "<<mean<<endl;
+    
+    return 0;
+}
