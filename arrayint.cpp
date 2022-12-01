ebb866543f8324ab8d043ea9ef1298a052f242f4
135777Derick <117374653+135777Derick@users.noreply.github.com>
2022-12-01 18:39:58 +0300

    Create arrayint.cpp

---
+++ arrayint.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nums[5];
+    cout<<"please enter 5 intergers: \n";
+    cin>>nums[0];
+    cin>>nums[1];
+    cin>>nums[2];
+    cin>>nums[3];
+    cin>>nums[4];
+    cout<<"the 5 values intergers entered are: "<<endl;
+    cout<<nums[0]<<endl;
+    cout<<nums[1]<<endl;
+    cout<<nums[2]<<endl;
+    cout<<nums[3]<<endl;
+    cout<<nums[4]<<endl;
+    return 0;
+}
